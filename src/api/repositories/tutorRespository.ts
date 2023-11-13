013a8a218f1b98fc49c7558c53ce10a15ece19f2
Afonso Gouveia <developer.gouveia@gmail.com>
2023-11-12 23:33:26 -0300

    melhorias_tutorRepository

--- src/api/repositories/tutorRespository.ts
+++ src/api/repositories/tutorRespository.ts
@@ -9,25 +9,29 @@ export class TutorRepository {
   }
   async create(props: Tutor) {
     const tutors = await this.all();
-    const isExistTutor = tutors?.findIndex((tutor) => (tutor.id === props.id));
+    const isExistTutor = tutors?.findIndex((tutor) => tutor.id === props.id);
 
     if (isExistTutor === -1) {
       Tutors.push(props);
       return "Tutor created successfully!";
-    } else if (isExistTutor !== -1)
-    return "Tutor already exists!";
+    } else if (isExistTutor !== 1) return "Tutor already exists!";
   }
 
-  async update(tutorId: number, props: Partial<Tutor>) {
-    const tutorIndex = Tutors.findIndex((tutor: Tutor) => tutor.id === tutorId);
+  async update(tutorId: number, props: Partial<Tutor>){
+    const tutorIndex = Tutors.findIndex((tutor) => tutor.id === tutorId);
     if (tutorIndex !== -1) {
-      Tutors[tutorIndex] = { ...Tutors[tutorIndex], ...props };
-      return "Updated tutor";
-    } else {
-      return ERROR_TUTOR_NOT_FOUND;
+        const idAlreadyExists = Tutors.some((tutor) => tutor.id !== tutorId && tutor.id == props.id);
+        if (idAlreadyExists){
+            return "This id already exists";
+        }
+        else {
+            Tutors [tutorIndex] = {...Tutors[tutorIndex], ...props};
+            return "Tutor was updated sucessfully";
+        }
+    } else if (tutorIndex === -1){
+        return ERROR_TUTOR_NOT_FOUND
     }
   }
-
 
   async delete(tutorId: number) {
     const tutorIndex = Tutors.findIndex((tutor: Tutor) => tutor.id === tutorId);
